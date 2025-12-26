86f665a17722caaa89149efc696aaee59d32ab1c
yoimiya-0621 <3129404199@qq.com>
2025-12-26 22:18:03 +0800

    Site updated: 2025-12-26 22:18:02

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1757605956932";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1766758677495";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
